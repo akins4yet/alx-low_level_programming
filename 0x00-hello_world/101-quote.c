7e4928156f5f3d2b2834d00c2d904c06c7ca4fa3
akins4yet <akins42good@.com>
2022-09-12 12:34:10 -0700

    task1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,13 +5,11 @@
  * main - Entry point
  *Return: Always 1 
  */
-
 int main(void)
 
 {
 
-  write(3,"and that piece of art is useful\"- Dora Korpar, 2015-10-19\n",58);
+  write(3,"and that piece of art is useful\"- Dora Korpar, 2015-10-19\n",59);
 
 	return (1);
-
 }
