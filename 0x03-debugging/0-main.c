c6e44a01217fb486401dafafc11cfb57727d8805
akins4yet <akins42good@.com>
2022-09-14 15:14:38 -0700

    task 0. Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,12 +3,9 @@
 
 
 /**
-
-* main - tests function that prints if integer is positive or negative
-
-* Return: 0
-
-*/
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
 
 
 
@@ -20,7 +17,7 @@ int main(void)
 
 
 
-        i = 98;
+        i = 0;
 
         positive_or_negative(i);
 
