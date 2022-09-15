1c4006ad3c505e64b31e91556800059bfc1dc79b
akins4yet <akins42good@.com>
2022-09-15 06:34:09 -0700

    task 0,12

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -23,17 +23,14 @@ int main(void)
 
         i = 0;
 
-
-
+/*
         while (i < 10)
 
         {
-
                 putchar(i);
-
         }
 
-
+*/
 
         printf("Infinite loop avoided! \\o/\n");
 
